89c87052284a8a4c243c8116b596454ac8f3aeea
Oksana Shik <shikoksana57@gmail.com>
2024-11-19 14:54:42 +0300

    add swagger config

---
+++ BookService/src/main/java/org/example/config/SwaggerConfig.java
@@ -0,0 +1,13 @@
+package org.example.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Book-service Api",
+                version = "0.0.1"
+        )
+)
+public class SwaggerConfig {
+}
