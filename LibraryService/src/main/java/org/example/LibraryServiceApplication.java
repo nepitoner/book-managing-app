3f3f0b519ef5399fb22cb651321cf873a554df65
Oksana Shik <shikoksana57@gmail.com>
2024-11-19 14:56:05 +0300

    add library application

---
+++ LibraryService/src/main/java/org/example/LibraryServiceApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryServiceApplication.class, args);
+    }
+}
